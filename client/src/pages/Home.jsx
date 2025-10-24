f4a52e8a7a8410be421c6a7d3701517b36d37792
umairafatima29 <umairafatima897@gmail.com>
2025-10-24 23:31:08 +0500

    fix error

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Header from '../components/header'
+import Header from '../components/Header'
 import ResetPassword from './ResetPassword'
 
 
