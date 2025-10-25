3503fadb4fb3e250c8898e31893981bf5a892200
umairafatima29 <umairafatima897@gmail.com>
2025-10-25 13:10:44 +0500

    update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,6 +9,7 @@ const Home = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-[url('/bg_img.png')] bg-cover bg-center">
         <Navbar/>
       <Header/>
+      <ResetPassword/>
       
 
     </div>
--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useRef, useContext } from "react";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
-import { AppContent } from "../context/AppContext";
 import axios from "axios";
 import { toast } from "react-toastify";
 
