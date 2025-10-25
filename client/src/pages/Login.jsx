1a0ae8423ad1d379cb23134b9a62b1352848980c
umairafatima29 <umairafatima897@gmail.com>
2025-10-25 12:24:52 +0500

    fix

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,36 +1,150 @@
-const onSubmitHandler = async (e) => {
-  e.preventDefault();
-  setIsLoading(true);
-  try {
-    axios.defaults.withCredentials = true;
-    if (state === 'signup') {
-      const { data } = await axios.post(backendUrl + '/api/auth/register', { name, email, password });
-      if (data.success) {
-        setIsLoggedIn(true);
-        
-        setTimeout(() => {
-          getUserData();
-        }, 100);
-        navigate('/');
+import React, { useContext, useState } from 'react';
+import { assets } from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
+import { AppContent } from '../context/AppContext';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const Login = () => {
+  const navigate = useNavigate();
+  const { backendUrl, setIsLoggedIn, getUserData } = useContext(AppContent);
+
+  const [state, setState] = useState('signup');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      axios.defaults.withCredentials = true;
+      if (state === 'signup') {
+        const { data } = await axios.post(backendUrl + '/api/auth/register', { name, email, password });
+        if (data.success) {
+          setIsLoggedIn(true);
+          setTimeout(() => {
+            getUserData();
+          }, 100);
+          navigate('/');
+        } else {
+          toast.error(data.message);
+        }
       } else {
-        toast.error(data.message);
+        const { data } = await axios.post(backendUrl + '/api/auth/login', { email, password });
+        if (data.success) {
+          setIsLoggedIn(true);
+          setTimeout(() => {
+            getUserData();
+          }, 100);
+          navigate('/');
+        } else {
+          toast.error(data.message);
+        }
       }
-    } else {
-      const { data } = await axios.post(backendUrl + '/api/auth/login', { email, password });
-      if (data.success) {
-        setIsLoggedIn(true);
-        
-        setTimeout(() => {
-          getUserData();
-        }, 100);
-        navigate('/');
-      } else {
-        toast.error(data.message);
-      }
+    } catch (error) {
+      toast.error(error.response?.data?.message || error.message);
+    } finally {
+      setIsLoading(false);
     }
-  } catch (error) {
-    toast.error(error.response?.data?.message || error.message);
-  } finally {
-    setIsLoading(false);
-  }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
+      <img
+        onClick={() => navigate('/')}
+        src={assets.logo}
+        alt=""
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
+      />
+
+      <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-small">
+        <h2 className="text-3xl font-semibold text-white text-center mb-3">
+          {state === 'signup' ? 'Create Account' : 'Login'}
+        </h2>
+
+        <p className="text-center text-sm mb-6">
+          {state === 'signup' ? 'Create your account' : 'Login to your account!'}
+        </p>
+
+        <form onSubmit={onSubmitHandler}>
+          {state === 'signup' && (
+            <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+              <img src={assets.person_icon} alt="" className="w-5 h-5" />
+              <input
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                className="bg-transparent outline-none"
+                type="text"
+                placeholder="Full Name"
+                required
+              />
+            </div>
+          )}
+
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.mail_icon} alt="" className="w-5 h-5" />
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              className="bg-transparent outline-none"
+              type="email"
+              placeholder="Email id"
+              required
+            />
+          </div>
+
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.lock_icon} alt="" className="w-5 h-5" />
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              className="bg-transparent outline-none"
+              type="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+
+          <p
+            onClick={() => navigate('/reset-password')}
+            className="mb-4 text-indigo-500 cursor-pointer"
+          >
+            Forgot Password?
+          </p>
+          <button
+            disabled={isLoading}
+            className="w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 text-white font-medium"
+          >
+            {isLoading ? 'Wait...' : (state === 'signup' ? 'Sign Up' : 'Login')}
+          </button>
+        </form>
+
+        {state === 'signup' ? (
+          <p className="text-gray-400 text-center text-xs mt-4">
+            Already have an account?{' '}
+            <span
+              onClick={() => setState('login')}
+              className="text-blue-400 cursor-pointer underline"
+            >
+              Login Here
+            </span>
+          </p>
+        ) : (
+          <p className="text-gray-400 text-center text-xs mt-4">
+            Don't have an account?{' '}
+            <span
+              onClick={() => setState('signup')}
+              className="text-blue-400 cursor-pointer underline"
+            >
+              Sign Up
+            </span>
+          </p>
+        )}
+      </div>
+    </div>
+  );
 };
+
+export default Login;
