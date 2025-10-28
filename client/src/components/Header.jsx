5898945216451217c6b3a4f7b0fb6c934da85b82
umairafatima29 <umairafatima897@gmail.com>
2025-10-28 18:57:05 +0500

    fix header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
         className="w-36 h-36 rounded-full mb-6"
       />
       <h1 className="flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2">
-        Hey {userData ? userData.name : "Developer"}!
+        Hey {userData ? userData.userName : "Developer"}!
         <img className="w-8 aspect-square" src={assets.hand_wave} alt="" />
       </h1>
       <h2 className="text-3xl sm:text-5xl font-semibold mb-2">
