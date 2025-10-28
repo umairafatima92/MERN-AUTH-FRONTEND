967485dde19550ff589bb51acd5db42ea97c8100
umairafatima29 <umairafatima897@gmail.com>
2025-10-28 18:51:54 +0500

    fix header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { AppContent } from "../context/AppContext";
 
 const Header = () => {
   const { userData } = useContext(AppContent);
-
+  console.log("User Data:", userData); 
   return (
     <div className="flex flex-col items-center mt-20 px-4 text-center text-gray-800">
       <img
@@ -14,8 +14,6 @@ const Header = () => {
       />
       <h1 className="flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2">
         Hey {userData ? userData.name : "Developer"}!
-        console.log(userData);
-        
         <img className="w-8 aspect-square" src={assets.hand_wave} alt="" />
       </h1>
       <h2 className="text-3xl sm:text-5xl font-semibold mb-2">
