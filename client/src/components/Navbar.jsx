34f73c856d463a24b65d6c8025c128d9c61dbd59
umairafatima29 <umairafatima897@gmail.com>
2025-10-28 19:05:17 +0500

    fix Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
       <img src={assets.logo} alt="logo" className="w-28 sm:w-32" />
       {userData ? (
         <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group">
-          {userData?.name?.[0]?.toUpperCase()}
+          {userData?.userName?.[0]?.toUpperCase()}
           <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
             <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
               {!userData?.isAccountVerified && (
